c61870d5e0d89eb3d4ad2d3c7414dc935dfb413e
Aniket Patil <official.aniketpatil@gmail.com>
2019-08-11 13:40:57 +0530

    removed style

--- navbar.php
+++ navbar.php
@@ -1,13 +1,13 @@
 <link rel="stylesheet" type="text/css" href="navbar.css">
 <nav class="mb-1 pt-2 pb-2 navbar navbar-expand-lg navbar-dark nav-color">
     <a class="navbar-brand" href="dashboard.php">
-        <img class="img-responsive" src="logo.png" hspace="12">Helping Nation</a>
     <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
         <spna class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-collapse collapse" id="navbarSupportedContent-4" style="">
         <ul class="navbar-nav ml-auto"> 
             <!-- <li class="nav-item active">
+                     <img class="img-responsive" src="logo.png" hspace="12">Helping Nation</a>
                     <a class="nav-link waves-effect waves-light" href="./index.php">
                     <i class="fa fa-home"></i> Dashboard
                     <span class="sr-only">(current)</span>

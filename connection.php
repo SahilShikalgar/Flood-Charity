6a598f4efe86c1e25297cc78a077214540381b98
Aniket Patil <official.aniketpatil@gmail.com>
2019-08-11 12:10:32 +0530

    connection modified

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-    $host = 'localhost';
-    // $name = 'u393528343_root';
-    // $pass = 'Cool@12345';
-    // $db = 'u393528343_helping_nation';
-    $name = 'root';
-    $pass = '';
-    $db = 'flood-relief';
+    //$host = 'localhost';
+    //$name = 'root';
+    //$pass = '';
+    //$db = 'flood-relief';
+    $name = 'u393528343_root';
+    $pass = 'Cool@12345';
+    $db = 'u393528343_helping_nation';
 ?>
